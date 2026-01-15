b03481617c9a6b3766bfc1505305118aa1f8bbc2
Akshaysajong <akshaysajong@gmail.com>
2026-01-15 13:50:39 +0530

    updated

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ env = environ.Env(
 )
 
 # Read .env file
-environ.Env.read_env(os.path.join(BASE_DIR.parent, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
